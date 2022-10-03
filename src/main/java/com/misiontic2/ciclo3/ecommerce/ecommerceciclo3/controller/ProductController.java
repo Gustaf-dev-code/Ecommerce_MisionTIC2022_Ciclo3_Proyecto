a00eecb4834ff6b177aaeb3bfe3abb3681a566fd
Gustavo Briceño <gustavo7490@gmail.com>
2022-10-03 13:35:40 -0500

    Prueba de creación de productos con LOGGER

--- src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/controller/ProductController.java
+++ src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/controller/ProductController.java
@@ -1,16 +1,41 @@
 package com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.controller;
 
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.model.entity.Product;
+import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service.ProductService;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
+    //Atributos
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService productService;
     
     //Métodos
     @GetMapping("")
     public String show(){
         return "products/show";
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "products/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product){
+        LOGGER.info("Este es el objeto producto {}",product);
+        productService.save(product);
+        return "redirect:/products";
     }
 }
