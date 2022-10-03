da7765ef0c6647d9dfda1f723c9ce62a8465d0c3
Gustavo Briceño <gustavo7490@gmail.com>
2022-10-03 09:59:20 -0500

    Creación de la vista productos y su controlador

---
+++ src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service;
+
+import java.util.Optional;
+
+import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.model.entity.Product;
+
+public interface ProductService {
+    
+    public Product save(Product product);
+    public Optional<Product> get(Integer id);
+    public void update(Product product);
+    public void delete(Integer id);
+}
---
+++ src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/service/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.model.entity.Product;
+import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.model.repositoryDAO.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    //Atributos
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product save(Product product) {   
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Optional<Product> get(Integer id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public void update(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.deleteById(id);
+    }
+    
+}
