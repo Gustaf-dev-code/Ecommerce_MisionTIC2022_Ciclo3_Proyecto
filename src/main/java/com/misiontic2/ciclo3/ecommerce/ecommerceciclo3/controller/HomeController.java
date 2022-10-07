c478861d81635d2fd9bb32d808ee60370dbeb391
Gustavo Briceño <gustavo7490@gmail.com>
2022-10-07 17:27:53 -0500

    Mostrar vista detalle del producto desde la home del usuario

--- src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/controller/HomeController.java
+++ src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service.ProductService;
@@ -13,6 +16,8 @@ import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service.ProductService;
 public class HomeController {
     
     //Atributos
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired //Inyecta al contenedor una instancia de tipo ProductService
     private ProductService productService;//Obtener los productos
 
@@ -23,5 +28,11 @@ public class HomeController {
 
         model.addAttribute("productos", productService.findAll());
         return "/user/home";
+    }
+
+    @GetMapping("productHome/{id}")
+    public String productHome(@PathVariable Integer id){
+        log.info("Id producto enviado como parámetro {}", id);
+        return "user/productohome";
     }
 }
