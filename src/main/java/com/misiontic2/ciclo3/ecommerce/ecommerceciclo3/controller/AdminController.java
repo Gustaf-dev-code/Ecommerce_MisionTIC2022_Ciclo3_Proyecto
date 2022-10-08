1377e7aaabbea2f05724bad647fa93d6f4f55ead
Gustavo Briceño <gustavo7490@gmail.com>
2022-10-08 13:54:52 -0500

    Funcionaliad para ver detalles de las ordenes para el admin

--- src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/controller/AdminController.java
+++ src/main/java/com/misiontic2/ciclo3/ecommerce/ecommerceciclo3/controller/AdminController.java
@@ -2,12 +2,16 @@ package com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.model.entity.Order;
 import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.model.entity.Product;
 import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service.IOrderService;
 import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service.IUserService;
@@ -18,6 +22,9 @@ import com.misiontic2.ciclo3.ecommerce.ecommerceciclo3.service.ProductService;
 public class AdminController {
     
     //Atributos
+
+    private final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
     @Autowired
     private ProductService productService;
 
@@ -46,5 +53,14 @@ public class AdminController {
     public String orders(Model model){
         model.addAttribute("ordenes", ordersService.findAll());
         return "admin/ordenes";
+    }
+
+    @GetMapping("/ordersDetails/{id}")
+    public String detalleOrden(Model model, @PathVariable Integer id){
+        logger.info("Id de la orden: {}", id);
+        Order order = ordersService.findById(id).get();
+
+        model.addAttribute("detalles", order.getDetail());
+        return "admin/detalleorden";
     }
 }
